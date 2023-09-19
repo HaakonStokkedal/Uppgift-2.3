139a667113900c6ccc8b3f7151b0eb2cca0e0a06
hast06001 <hast06001@E-PW024NW1>
2023-09-19 21:37:18 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_2._3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur många kilometer ska du köra med bilen?");
+            string kilometerText = Console.ReadLine();
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            string dagarText = Console.ReadLine();
+            int kilometer = int.Parse(kilometerText);
+            int dagar = int.Parse(dagarText);
+            long totalt = 300 + (dagar * 500) - 500 + kilometer; 
+            Console.WriteLine("Det kommer kosta " +  totalt + " kr.");
+
+            Console.ReadKey();
+        }
+    }
+}
